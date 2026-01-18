1d00bead50bf7ba1c1aecac76319edb721d2e91a
GabrielHope <gabrielhopeofficial@proton.me>
2026-01-18 16:36:25 +0800

    Ready Product to ship to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import rss from '@astrojs/rss';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig(async () => {
-  site: 'http://localhost:4321',
   const mdxIntegration = await mdx();
   const rssIntegration = await rss();
 
   return {
-    site: 'http://localhost:4321', 
+    site: 'http://localhost:4321',  // ← moved inside the returned object
     output: 'static',
-    integrations: [mdxIntegration, rssIntegration],
+    integrations: [
+     mdx(),
+     rss(),
+     tailwind(),],
   };
 });
