f4f799bfdc67e78cd685f395851cee28aae86b87
GabrielHope <gabrielhopeofficial@proton.me>
2026-03-18 18:54:19 +0800

    Added a new post(distractions) and updated the date previews.

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,5 @@
 
+import __ASTRO_IMAGE_IMPORT_ZlYgzy from "../../assets/earpods.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2FDistractedtoDeath.mdx";
 import __ASTRO_IMAGE_IMPORT_1Uay8B from "../../assets/baptism.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fbaptism.mdx";
-import __ASTRO_IMAGE_IMPORT_1r20Yr from "../../assets/slow-learning.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fslow-learning-frontend.md";
-export default new Map([["../../assets/baptism.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fbaptism.mdx", __ASTRO_IMAGE_IMPORT_1Uay8B], ["../../assets/slow-learning.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fslow-learning-frontend.md", __ASTRO_IMAGE_IMPORT_1r20Yr]]);
+export default new Map([["../../assets/earpods.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2FDistractedtoDeath.mdx", __ASTRO_IMAGE_IMPORT_ZlYgzy], ["../../assets/baptism.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fbaptism.mdx", __ASTRO_IMAGE_IMPORT_1Uay8B]]);
 		
--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
+["src/content/blog/DistractedtoDeath.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FDistractedtoDeath.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/baptism.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbaptism.mdx&astroContentModuleFlag=true")]]);
 		
