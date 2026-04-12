36389a2f968e7b0b9da772b3f8a83aab6373b20f
GabrielHope <gabrielhopeofficial@proton.me>
2026-04-12 22:46:45 +0800

    Added the new post and changed up the layouts inside the blogs

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,6 @@
 
+import __ASTRO_IMAGE_IMPORT_Z1KGHkh from "../../assets/sleeper.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2FIStoppedTryingtoBeSureIWasSaved.mdx";
 import __ASTRO_IMAGE_IMPORT_ZlYgzy from "../../assets/earpods.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2FDistractedtoDeath.mdx";
 import __ASTRO_IMAGE_IMPORT_1Uay8B from "../../assets/baptism.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fbaptism.mdx";
-export default new Map([["../../assets/earpods.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2FDistractedtoDeath.mdx", __ASTRO_IMAGE_IMPORT_ZlYgzy], ["../../assets/baptism.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fbaptism.mdx", __ASTRO_IMAGE_IMPORT_1Uay8B]]);
+export default new Map([["../../assets/sleeper.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2FIStoppedTryingtoBeSureIWasSaved.mdx", __ASTRO_IMAGE_IMPORT_Z1KGHkh], ["../../assets/earpods.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2FDistractedtoDeath.mdx", __ASTRO_IMAGE_IMPORT_ZlYgzy], ["../../assets/baptism.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fbaptism.mdx", __ASTRO_IMAGE_IMPORT_1Uay8B]]);
 		
