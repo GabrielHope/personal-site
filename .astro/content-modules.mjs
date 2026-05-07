b945d3f755afca960fef4bfa68610cd0f075e9be
GabrielHope <gabrielhopeofficial@proton.me>
2026-05-07 13:19:39 +0800

    New post 'I was Convinced of Orthodoxy Long Before I followed it

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,7 @@
 
 export default new Map([
+["src/content/blog/ConvincedBeforeMoved.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FConvincedBeforeMoved.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/IStoppedTryingtoBeSureIWasSaved.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FIStoppedTryingtoBeSureIWasSaved.mdx&astroContentModuleFlag=true")],
-["src/content/blog/DistractedtoDeath.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FDistractedtoDeath.mdx&astroContentModuleFlag=true")],
-["src/content/blog/baptism.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbaptism.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/baptism.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbaptism.mdx&astroContentModuleFlag=true")],
+["src/content/blog/DistractedtoDeath.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FDistractedtoDeath.mdx&astroContentModuleFlag=true")]]);
 		
